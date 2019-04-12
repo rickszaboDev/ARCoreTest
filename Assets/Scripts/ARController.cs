afd2bd89a7ddb36399fb2d065940ff5914892a4d
Rick Szabo Jr <rick.szabo.jr@gmail.com>
2019-04-12 12:14:50 -0300

    part3 end

--- Assets/Scripts/ARController.cs
+++ Assets/Scripts/ARController.cs
@@ -7,7 +7,11 @@ using GoogleARCore;
 public class ARController : MonoBehaviour {
 
     private List<TrackedPlane> m_NewTrackedPlanes = new List<TrackedPlane>();
+
     public GameObject GridPrefab;
+    public GameObject Portal;
+
+    public GameObject ARCamera;
 
 	// Use this for initialization
 	void Start () {
@@ -32,6 +36,43 @@ public class ARController : MonoBehaviour {
 
             // This function will set the position of grid and modify the vertices of the attached mesh.
             grid.GetComponent<GridVisualizer>().Initialize(m_NewTrackedPlanes[i]);
+        }
+
+        //Check if user touches the screen.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        //Let's now check if the user touched any of the tracked planes
+        TrackableHit hit;
+        if(Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon, out hit))
+        {
+            // Let's now place the portal on the portal on top of the tracked plane that we touched.
+
+            // Enable the portal.
+            Portal.SetActive(true);
+
+            //Create new Anchor.
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+            //Set the position of the portal to be the same as the hit position
+            Portal.transform.position = hit.Pose.position;
+            Portal.transform.rotation = hit.Pose.rotation;
+
+            //We want the portal to face the camera
+            Vector3 cameraPosition = ARCamera.transform.position;
+
+            //The portal should only rotate around the Y axis
+            cameraPosition.y = hit.Pose.position.y;
+
+            //Rotate the portal to face the camera
+            Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+
+            //ARcore will keep understanding the woeld and update the anchors accordingly hence we need to attach our portal to the anchor
+            Portal.transform.parent = anchor.transform;
+
         }
 	}
 }
