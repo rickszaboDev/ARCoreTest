adc4031a695a1b901958456ac274368e810cca71
Rick Szabo Jr <rick.szabo.jr@gmail.com>
2019-05-15 10:36:46 -0300

    refactoring project - reordering folders

--- Assets/Scripts/Cubes/Direction.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Direction : MonoBehaviour {
-	public enum Cube {Left, Right, Front, Back, Up, Down, None};
-}
